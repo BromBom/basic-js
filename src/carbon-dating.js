839b325ca5dbca6fc2f9d0b0c73168885dddc712
Ihar Levachkov <pokerlis@gmail.com>
2023-12-23 14:31:37 +0300

    feat: add solution carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if ((!sampleActivity)
+    || (typeof sampleActivity !== 'string')
+    || (isNaN(+sampleActivity / 2))
+    || (+sampleActivity <= 0)
+    || (+sampleActivity > 15)) {
+    return false;
+  }
+  let age = -5730 * Math.log2(+sampleActivity / 15);
+  return Math.ceil(age);
 }
 
 module.exports = {
